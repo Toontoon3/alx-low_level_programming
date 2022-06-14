12f51f3247e6b90c8f8a9f4a97343d59291e9713
Olanrewaju <yinkagoodness@gmail.com>
2022-06-13 19:18:21 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 	for (; i < n; i++)
-
 	{
-		printf("%d", *(a + i));
+	printf("%d", *(a + i));
 
-		if (i != (n - 1))
-			printf(", ");
+	if (i != (n - 1))
+	
+	printf(", ");
 	}
 	printf("\n");
 }
